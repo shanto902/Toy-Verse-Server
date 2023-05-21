d6a42f0817aa11c1e9eeaac58e0b3c2017263fa2
Ashik Ali Shanto <shanto902@gmail.com>
2023-05-21 16:02:52 +0600

    single toy details view path added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -25,12 +25,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toy-verse').collection('toys')
+    app.get('/toys', async (req, res) => {
+        const cursor = toyCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/toy/:id', async (req,res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+
+      const result = await toyCollection.findOne(query)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+ 
   }
 }
 run().catch(console.dir);
